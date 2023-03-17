50ab2123fab67bceafa934e5c43d8cb6403f9084
Livqonn <88245375+KaygusuzAbdal@users.noreply.github.com>
2023-03-17 20:45:42 +0300

    Update 2d-walk.py

--- 2d-walk.py
+++ 2d-walk.py
@@ -8,22 +8,26 @@ def start_walking(total_steps=500):
  timepoints = list(range(0, total_steps+1))
  
  for number in range(1, total_steps + 1):
- possibility = random.randint(1, 10)
- if possibility <= 2:
- #left
- x -= 1
- elif 4 >= possibility > 2:
- #right
- x += 1
- elif 9 >= possibility > 4:
- #up
- y += 1
- else:
- #down
- y -= 1
- positions.append([x/10, y/10])
- return timepoints, positions
+  possibility = random.randint(1, 10)
+  
+  if possibility <= 2:
+   #left (possibility 20%)
+   x -= 1
+  elif 4 >= possibility > 2:
+   #right (possibility 20%)
+   x += 1
+  elif 9 >= possibility > 4:
+   #up (possibility 50%)
+   y += 1
+  else:
+   #down (possibility 10%)
+   y -= 1
+  
+  positions.append([x/10, y/10])
+  return timepoints, positions
+
 time_points, positions = start_walking(500)
+
 print(time_points)
 print(positions)
 print(positions[500])
